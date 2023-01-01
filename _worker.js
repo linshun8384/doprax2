3c2fb812ca077a63fff82e48fd5718682f4adce8
linshun8384 <101203582+linshun8384@users.noreply.github.com>
2023-01-01 17:31:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="dopraxtz59.eu-ccofhtfzmvsd.dopraxapp.com";
+        url.hostname="lys523d0t.eu-ccofhtfzmvsd.dopraxapp.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
